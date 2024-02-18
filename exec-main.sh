1e405c3f4994f33f4eb4e6e67a277986389245ff
ThunderSmoker <pitlawararadhya@gmail.com>
2024-02-18 22:56:50 +0530

    delete file

--- exec-main.sh
+++ exec-main.sh
@@ -6,5 +6,17 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-
-go run main.go
+# Check the first argument provided
+if [ "$1" == "ls" ]; then
+  go run main.go
+elif [ "$1" == "rm" ]; then
+  # Check if filename is provided as the second argument
+  if [ -z "$2" ]; then
+    echo "Filename not provided with 'rm' command. Exiting..."
+    exit 1
+  fi
+  go run main.go "$2"
+else
+  echo "Unknown command: $1"
+  exit 1
+fi
